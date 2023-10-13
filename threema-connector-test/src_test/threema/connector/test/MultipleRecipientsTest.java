1d7bed65087076d1da79c6bacbb68738398a97fb
Fabian Heuberger <fabian.heuberger@axonivy.com>
2023-10-13 16:37:20 +0200

    added tests for singleRecipient and multipleRecipients processes

---
+++ threema-connector-test/src_test/threema/connector/test/MultipleRecipientsTest.java
@@ -0,0 +1,47 @@
+package threema.connector.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.scripting.objects.List;
+import threema.connector.receiverData;
+import threema.connector.sendThreemaMessageData;
+import util.LookupType;
+
+@IvyProcessTest
+public class MultipleRecipientsTest {
+
+	private final static BpmProcess MULTIPLE_RECIPIENTS_PROCESS = BpmProcess.name("multipleRecipients");
+	private final static String MESSAGE = "Hello World";
+	
+	
+	@Test
+	void prepareMultipleRecipients(BpmClient bpmClient) {
+		List<String> recipients = new List<String>();
+		recipients.add("invalidThreemaID");
+		recipients.add("41000000000");
+		recipients.add("invalid@email.ch");
+		
+		BpmElement callable = MULTIPLE_RECIPIENTS_PROCESS.elementName("call(String,List<String>)");	
+		ExecutionResult result = bpmClient.start().subProcess(callable).execute(MESSAGE, recipients);
+		sendThreemaMessageData msgData = result.data().last();
+		List<receiverData> listReceiver = msgData.getReceiverData();
+		
+		assertThat(listReceiver.size()).isEqualTo(recipients.size());
+		assertThat(listReceiver.get(0).getType()).isEqualTo(LookupType.THREEMAID);
+		assertThat(listReceiver.get(1).getType()).isEqualTo(LookupType.PHONE);
+		assertThat(listReceiver.get(2).getType()).isEqualTo(LookupType.EMAIL);
+		
+		for(String status : msgData.getApiResponses()) {
+			assertThat(status).contains("404");
+		}
+
+	}
+	
+}
---
+++ threema-connector-test/src_test/threema/connector/test/SingleRecipientTest.java
@@ -0,0 +1,55 @@
+package threema.connector.test;
+
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import threema.connector.receiverData;
+
+@IvyProcessTest
+public class SingleRecipientTest {
+
+	private static final BpmProcess SINGLE_RECEIVER_PROCESS = BpmProcess.name("singleRecipient");
+	private static final String VALID_ID = "ECHOECHO";
+	private static final String INVALID_EMAIL = "invalid@mail.ch";
+	private static final String INVALID_PHONE = "41000000000";
+	private static final String MESSAGE = "Hello World";
+	
+	@Test
+	void sendMessageToValidSingleRecipientById(BpmClient bpmClient) {
+		BpmElement callable = SINGLE_RECEIVER_PROCESS.elementName("call(String,String,LookupType)");
+		ExecutionResult result = bpmClient.start().subProcess(callable).execute(MESSAGE, VALID_ID, util.LookupType.THREEMAID);
+		receiverData resultData = result.data().last();
+		String apiStatus = resultData.getApiResponse();
+		
+		assertThat(apiStatus).contains("200");
+	}
+	
+	@Test
+	void sendMessageToInvalidSingleRecipientByEmail(BpmClient bpmClient) {
+		BpmElement callable = SINGLE_RECEIVER_PROCESS.elementName("call(String,String,LookupType)");
+		ExecutionResult result = bpmClient.start().subProcess(callable).execute(MESSAGE, INVALID_EMAIL, util.LookupType.EMAIL);
+		receiverData resultData = result.data().last();
+		String apiStatus = resultData.getApiResponse();
+		
+		assertThat(apiStatus).contains("404");
+	}
+	
+	@Test
+	void sendMessageToInvalidSingleRecipientByPhone(BpmClient bpmClient) {
+		BpmElement callable = SINGLE_RECEIVER_PROCESS.elementName("call(String,String,LookupType)");
+		ExecutionResult result = bpmClient.start().subProcess(callable).execute(MESSAGE, INVALID_PHONE, util.LookupType.PHONE);
+		receiverData resultData = result.data().last();
+		String apiStatus = resultData.getApiResponse();
+		
+		assertThat(apiStatus).contains("404");
+	}
+	
+}
