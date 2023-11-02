71f43d5a60be2d89be58039d730000ebbd032c92
Fabian Heuberger <fabian.heuberger@axonivy.com>
2023-11-02 12:13:42 +0100

    XIVY-12667 Upgraded buildplugin to 10.0.14 enabled and configured webtests

--- threema-connector-webtest/src_test/threema/connector/webtest/MessageMultipleRecipientsTest.java
+++ threema-connector-webtest/src_test/threema/connector/webtest/MessageMultipleRecipientsTest.java
@@ -7,20 +7,23 @@ import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
+import com.axonivy.ivy.webtest.engine.WebAppFixture;
 import com.codeborne.selenide.ElementsCollection;
+
+import ch.ivyteam.threema.mocks.ThreemaServiceMock;
 
 @IvyWebTest
 public class MessageMultipleRecipientsTest {
 
   @Test
-  @Disabled
-  public void sendMessage() {
+  public void sendMessage(WebAppFixture fixture) {
+    String mockUrl = ThreemaServiceMock.URI.replaceAll("\\{", "%7B").replaceAll("\\}", "%7D").replaceAll("/", "%2F");
+    fixture.config("RestClients.ThreemaGateway.Url", mockUrl);
     open(EngineUrl.createProcessUrl("threema-connector-demo/18B8EEA3B9A84FAE/SendMessageToMultipleRecipients.ivp"));
     String message = "Hello World";
     String recipients = "validId\ninvalidId";
--- threema-connector-webtest/src_test/threema/connector/webtest/MessageSingleRecipientTest.java
+++ threema-connector-webtest/src_test/threema/connector/webtest/MessageSingleRecipientTest.java
@@ -8,21 +8,24 @@ import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
+import com.axonivy.ivy.webtest.engine.WebAppFixture;
 import com.axonivy.ivy.webtest.primeui.PrimeUi;
 import com.codeborne.selenide.ElementsCollection;
+
+import ch.ivyteam.threema.mocks.ThreemaServiceMock;
 
 @IvyWebTest
 public class MessageSingleRecipientTest {
 
   @Test
-  @Disabled
-  public void sendMessage() {
+  public void sendMessage(WebAppFixture fixture) {
+    String mockUrl = ThreemaServiceMock.URI.replaceAll("\\{", "%7B").replaceAll("\\}", "%7D").replaceAll("/", "%2F");
+    fixture.config("RestClients.ThreemaGateway.Url", mockUrl);
     open(EngineUrl.createProcessUrl("threema-connector-demo/18B8EEA3B9A84FAE/SendMessageToSingleRecipient.ivp"));
     String message = "Hello World";
     String validId = "validId";
