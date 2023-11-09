06c43d9899bb3c28d98fcd9fd35e7f57f24536bc
Fabian Heuberger <fabian.heuberger@axonivy.com>
2023-11-09 10:02:05 +0100

    XIVY-12667 changed webtests to Integrationtests

--- threema-connector-webtest/src_test/threema/connector/webtest/MessageMultipleRecipientsIT.java
+++ threema-connector-webtest/src_test/threema/connector/webtest/MessageMultipleRecipientsIT.java
@@ -1,7 +1,6 @@
 package threema.connector.webtest;
 
 import static com.codeborne.selenide.Condition.empty;
-import static com.codeborne.selenide.Condition.selected;
 import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -15,43 +14,38 @@ import org.openqa.selenium.By;
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 import com.axonivy.ivy.webtest.engine.WebAppFixture;
-import com.axonivy.ivy.webtest.primeui.PrimeUi;
 import com.codeborne.selenide.ElementsCollection;
 
 import ch.ivyteam.threema.mocks.ThreemaServiceMock;
 
 @IvyWebTest
-public class MessageSingleRecipientTest {
+public class MessageMultipleRecipientsIT {
 
   @AfterEach
   public void cleanFixture(WebAppFixture fixture) {
     fixture.resetConfig("RestClients.ThreemaGateway.Url");
   }
 
-
   @Test
   public void sendMessage(WebAppFixture fixture) {
     String mockUrl = ThreemaServiceMock.URI.replaceAll("\\{", "%7B").replaceAll("\\}", "%7D").replaceAll("/", "%2F");
     fixture.config("RestClients.ThreemaGateway.Url", mockUrl);
-    open(EngineUrl.createProcessUrl("threema-connector-demo/18B8EEA3B9A84FAE/SendMessageToSingleRecipient.ivp"));
+    open(EngineUrl.createProcessUrl("threema-connector-demo/18B8EEA3B9A84FAE/SendMessageToMultipleRecipients.ivp"));
     String message = "Hello World";
-    String validId = "validId";
+    String recipients = "validId\ninvalidId";
     // Assert empty form
     $(By.id("form:sendDemoMessageDataPlainMessage")).shouldBe(empty);
     $(By.id("form:sendDemoMessageDataReceiver")).shouldBe(empty);
-    $(By.id("form:typeSelection:2")).shouldNotBe(selected);
     // Proceed without required fields
     $(By.id("form:proceed")).click();
     // Assert all fields required
     ElementsCollection errorMessages = $$(By.cssSelector(".ui-state-error"));
-    assertThat(errorMessages).hasSize(8);
+    assertThat(errorMessages).hasSize(4);
     // Fill out form
     $(By.id("form:sendDemoMessageDataPlainMessage")).sendKeys(message);
-    $(By.id("form:sendDemoMessageDataReceiver")).sendKeys(validId);
-    PrimeUi.selectOneRadio(By.id("form:typeSelection")).selectItemByValue("threemaid");
+    $(By.id("form:sendDemoMessageDataReceiver")).sendKeys(recipients);
     // Assert filled out form
     $(By.id("form:sendDemoMessageDataPlainMessage")).shouldBe(value(message));
-    $(By.id("form:sendDemoMessageDataReceiver")).shouldBe(value(validId));
-    $(By.id("form:typeSelection:2")).shouldBe(selected);
+    $(By.id("form:sendDemoMessageDataReceiver")).shouldBe(value(recipients));
   }
 }
--- threema-connector-webtest/src_test/threema/connector/webtest/MessageSingleRecipientIT.java
+++ threema-connector-webtest/src_test/threema/connector/webtest/MessageSingleRecipientIT.java
@@ -1,6 +1,7 @@
 package threema.connector.webtest;
 
 import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.selected;
 import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -14,38 +15,43 @@ import org.openqa.selenium.By;
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.engine.EngineUrl;
 import com.axonivy.ivy.webtest.engine.WebAppFixture;
+import com.axonivy.ivy.webtest.primeui.PrimeUi;
 import com.codeborne.selenide.ElementsCollection;
 
 import ch.ivyteam.threema.mocks.ThreemaServiceMock;
 
 @IvyWebTest
-public class MessageMultipleRecipientsTest {
+public class MessageSingleRecipientIT {
 
   @AfterEach
   public void cleanFixture(WebAppFixture fixture) {
     fixture.resetConfig("RestClients.ThreemaGateway.Url");
   }
 
+
   @Test
   public void sendMessage(WebAppFixture fixture) {
     String mockUrl = ThreemaServiceMock.URI.replaceAll("\\{", "%7B").replaceAll("\\}", "%7D").replaceAll("/", "%2F");
     fixture.config("RestClients.ThreemaGateway.Url", mockUrl);
-    open(EngineUrl.createProcessUrl("threema-connector-demo/18B8EEA3B9A84FAE/SendMessageToMultipleRecipients.ivp"));
+    open(EngineUrl.createProcessUrl("threema-connector-demo/18B8EEA3B9A84FAE/SendMessageToSingleRecipient.ivp"));
     String message = "Hello World";
-    String recipients = "validId\ninvalidId";
+    String validId = "validId";
     // Assert empty form
     $(By.id("form:sendDemoMessageDataPlainMessage")).shouldBe(empty);
     $(By.id("form:sendDemoMessageDataReceiver")).shouldBe(empty);
+    $(By.id("form:typeSelection:2")).shouldNotBe(selected);
     // Proceed without required fields
     $(By.id("form:proceed")).click();
     // Assert all fields required
     ElementsCollection errorMessages = $$(By.cssSelector(".ui-state-error"));
-    assertThat(errorMessages).hasSize(4);
+    assertThat(errorMessages).hasSize(8);
     // Fill out form
     $(By.id("form:sendDemoMessageDataPlainMessage")).sendKeys(message);
-    $(By.id("form:sendDemoMessageDataReceiver")).sendKeys(recipients);
+    $(By.id("form:sendDemoMessageDataReceiver")).sendKeys(validId);
+    PrimeUi.selectOneRadio(By.id("form:typeSelection")).selectItemByValue("threemaid");
     // Assert filled out form
     $(By.id("form:sendDemoMessageDataPlainMessage")).shouldBe(value(message));
-    $(By.id("form:sendDemoMessageDataReceiver")).shouldBe(value(recipients));
+    $(By.id("form:sendDemoMessageDataReceiver")).shouldBe(value(validId));
+    $(By.id("form:typeSelection:2")).shouldBe(selected);
   }
 }
